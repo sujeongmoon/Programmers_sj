8c9fab8de78ce967e5848959864c60427230403f
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2024-04-30 09:08:10 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,6 +1,22 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        
+        /* case1) 그냥 나머지 0인거 바로 계산, i는 2
+        for (int i = 2; i <= n; i++){
+            if (i%2 ==0){
+                answer += i;
+            }
+        }
+        */
+        
+        
+        //case2) n을 반으로 나누고 1부터 n/2까지 다 더한 다음 *2하는 것도 가능.
+        
+        for (int i = 1; i <= n/2; i++){
+            answer += i;
+        }
+        answer *= 2;
         return answer;
     }
 }
