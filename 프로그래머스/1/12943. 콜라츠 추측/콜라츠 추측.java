11854714160dcc0e4e21bf9538e06520be188ebe
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2024-05-17 15:17:35 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.15 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        
+        if (num == 1) {
+            return 0;
+        }
+        
+        while (num != 1) {
+            
+            if (answer == 500) {
+                return -1;
+            }
+            if (num % 2 == 0) {
+                num /= 2;
+            } else if (num % 2 == 1) {
+                num *= 3;
+                num += 1;
+            }
+            answer += 1;
+        }
+        
+        System.out.println(num);
+        return answer;
+    }
+}
