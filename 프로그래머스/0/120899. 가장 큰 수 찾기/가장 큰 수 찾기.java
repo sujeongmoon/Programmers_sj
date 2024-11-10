b2c4722a536e5340d807f105e4f66e991600a7f3
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2024-11-10 23:28:21 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = {0, 0};
+        //int max = 0;
+        
+        for (int i = 0; i < array.length; i++){
+            if(array[i] > answer[0]){
+                answer[0] = array[i];
+                answer[1] = i;
+            }
+        }
+        
+        return answer;
+    }
+}
