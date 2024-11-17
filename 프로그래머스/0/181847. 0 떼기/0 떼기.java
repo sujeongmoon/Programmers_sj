1a795598fdf26e6b04f8ef76bbf646f9adb3e5ca
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2024-11-17 23:41:41 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 67.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        
+        int n_int = Integer.parseInt(n_str);
+        answer = String.valueOf(n_int);
+        
+        return answer;
+    }
+}
